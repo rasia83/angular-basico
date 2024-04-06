ad534bf8c172367cf308e0927034793e85b3176b
Tiago Edgar Rasia <103585675+rasia83@users.noreply.github.com>
2024-04-06 17:43:39 -0300

    Aulas 10 e 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,15 @@ import { Componente02Component } from "./componente02/componente02.component";
 import { Componente03Component } from "./componente03/componente03.component";
 import { Componente04Component } from "./componente04/componente04.component";
 import { Componente05Component } from "./componente05/componente05.component";
+import { Componente06Component } from "./componente06/componente06.component";
+import { Componente07Component } from "./componente07/componente07.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component]
+    imports: [CommonModule, RouterOutlet, Componente01Component, Componente02Component, Componente03Component, Componente04Component, Componente05Component, Componente06Component, Componente07Component]
 })
 export class AppComponent {
   title = 'angular-basico';
