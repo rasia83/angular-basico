823a0a1abc9b7473616486466bcb6084c83967e0
Tiago Edgar Rasia <103585675+rasia83@users.noreply.github.com>
2024-04-09 10:43:27 -0300

    Aula 26-29

--- src/app/componente11/componente11.component.ts
+++ src/app/componente11/componente11.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-componente11',
@@ -11,8 +11,8 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class Componente11Component {
 
   formulario = new FormGroup({
-    nome : new FormControl(''),
-    idade : new FormControl(null),
-    cidade : new FormControl('')
+    nome : new FormControl('', [Validators.required, Validators.minLength(3)]),
+    idade : new FormControl(null, [Validators.required, Validators.min(0), Validators.max(120)]),
+    cidade : new FormControl('', [Validators.required, Validators.minLength(3)])
   })
 }
